ab685a3febc31776451e2c02c1945afaf501082e
julianbarg <barg.julian@gmail.com>
2019-03-03 17:49:36 -0500

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='wrds_tools',
+      version='0.0.1',
+      description='Various tools to create a connection to the WRDS service and download commonly used data.',
+      author='Julian Barg',
+      author_email='barg.julian@gmail.com',
+      packages=['wrds_tools'],
+      install_requires=['pandas']
+      )
---
+++ wrds_tools/wrds_connection.py
@@ -0,0 +1,7 @@
+import wrds
+
+
+class WrdsConnection:
+    """A connection to the WRDS database. Saves username and password for you and builds connection.
+
+    """

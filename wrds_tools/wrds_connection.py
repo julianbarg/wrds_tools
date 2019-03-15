0a94719de58aa0b9e8869827eb705b641659c746
julianbarg <barg.julian@gmail.com>
2019-03-15 17:53:53 -0400

    Added option to add executives.

--- wrds_tools/wrds_connection.py
+++ wrds_tools/wrds_connection.py
@@ -1,6 +1,8 @@
-import wrds
+import wrds as _wrds
 import pandas as pd
+import warnings
 
+from itertools import compress
 from pandas import DataFrame
 from datetime import date
 import numpy as np
@@ -28,27 +30,35 @@ class NoDatasetError(AttributeError):
 class WrdsConnection:
     """
     A connection to the WRDS database. Saves username and password for you and builds connection. Observation period can
-    be set when initiating the connection object, or can explicitely be set later by using the set_observation_period
+    be set when initiating the connection object, or can explicitly be set later by using the set_observation_period
     method.
 
     :param wrds_username: Your wrds account username. Username must match with the username specified in the .pgpass
-                          file on your computer. If no username is specified, will look up username from file
-                          specified in parameters.txt in the project directory.
-    :param start_date: datetime.date instance with first day of observa`tion period.
-    :param end_date: datetime.date instance with last day of observation period.
+    file on your computer. If no username is specified, will look up username from file specified in parameters.txt in
+    the project directory.
+    :param selection_start_date: datetime.date, start of time period within which companies will be selected into the
+    sample.
+    :param selection_end_date: datetime.date, end of time period within which companies will be selected into the
+    sample.
+    :param observation_start_date: datetime.date instance with first day of observation period.
+    :param observation_end_date: datetime.date instance with last day of observation period.
     :ivar db: Saves your connection to the wrds database.
     :ivar dataset: Dataset of Panda DataFrame type that holds the data extracted from WRDS.
     :return: An object that holds the wrds connection object.
     """
-    def __init__(self, wrds_username: str, start_date: date = None, end_date: date = None):
+    def __init__(self, wrds_username: str, selection_start_date: date = None, selection_end_date: date = None,
+                 observation_start_date: date = None, observation_end_date: date = None):
         self.username = wrds_username
 
-        self.start_date = start_date
-        self.end_date = end_date
+        self.selection_start_date = selection_start_date
+        self.selection_end_date = selection_end_date
+        self.observation_start_date = observation_start_date
+        self.observation_end_date = observation_end_date
 
         self.dataset = None
         self._names_table = None
         self._company_table = None
+        self._executive_table = None
 
         # To build a connection to the wrds server via python, a .pgpass file is required in the user's home
         # directory, with access limited to the user.
@@ -57,7 +67,18 @@ class WrdsConnection:
         # After creating the file, don't forget to run "chmod 0600 ~/.pgpass" in the console to limit access.
         # Access issue also described here:
         # https://www.postgresql.org/docs/9.5/libpq-pgpass.html.
-        self.db = wrds.Connection(wrds_username=self.username)
+        self.db = _wrds.Connection(wrds_username=self.username)
+
+    def set_selection_period(self, start_date: date = None, end_date: date = None):
+        """
+        Explicitely set the observation period by passing in a datetime object. If no start date is provided, sample
+        will include all observations since beginning of recording. If no end date is provided, sample will include all
+        observations until the present.
+        :param start_date: datetime.date instance with first day of observation period.
+        :param end_date: datetime.date instance with last day of observation period.
+        """
+        self.selection_start_date = start_date
+        self.selection_end_date = end_date
 
     def set_observation_period(self, start_date: date = None, end_date: date = None):
         """
@@ -67,8 +88,8 @@ class WrdsConnection:
         :param start_date: datetime.date instance with first day of observation period.
         :param end_date: datetime.date instance with last day of observation period.
         """
-        self.start_date = start_date
-        self.end_date = end_date
+        self.observation_start_date = start_date
+        self.observation_end_date = end_date
 
     def build_sp500(self, rename_columns=True, drop_uninformative=True):
         """
@@ -86,24 +107,30 @@ class WrdsConnection:
         # "000003" is the Global Index Key of the S&P 500.
         dataset_raw = constituents_all_indexes[constituents_all_indexes.gvkeyx == '000003']
 
-        if (self.start_date is not None) and (self.end_date is not None):
+        # Filter observations by removing companies that joined/were dropped from the S&P 500 after/before the selection
+        # period.
+        if (self.selection_start_date is not None) and (self.selection_end_date is not None):
             # We filter for observations that fall within the observation period by identifying index constituents that
             # either dropped out before the observation period, or joined after, and then taking all companies that do
             # not match one of those two cases.
-            after = dataset_raw['from'] >= self.end_date
-            before = dataset_raw['thru'] <= self.start_date
+            after = dataset_raw['from'] >= self.selection_end_date
+            before = dataset_raw['thru'] <= self.selection_start_date
             self.dataset = dataset_raw[~before & ~after]
-        elif self.start_date is not None:
-            before = dataset_raw['thru'] <= self.start_date
+        # If only a start data is selected, we filter only by excluding companies that left the index before the start
+        # date.
+        elif self.selection_start_date is not None:
+            before = dataset_raw['thru'] <= self.selection_start_date
             self.dataset = dataset_raw[~before]
-        elif self.end_date is not None:
-            after = dataset_raw['from'] >= self.end_date
+        # If a selection end date is provided, remove companies that joined the S&P 500 after the selection period
+        # ended.
+        elif self.selection_end_date is not None:
+            after = dataset_raw['from'] >= self.selection_end_date
             self.dataset = dataset_raw[~after]
         else:
-            self.dataset = dataset_raw
+            self.dataset = dataset_raw.copy()
 
-        # Some companies are dropped from the S&P 500 and later join again. This leads to duplicates in the data that
-        # we filter out here. The last observation is retained (we expect observations to be equal).
+        # Some companies are dropped from the S&P 500 and later join again. This leads to duplicates in the data that we
+        # filter out here. The last observation is retained (we expect observations to be equal).
         self.dataset = self.dataset.drop_duplicates(subset='gvkey', keep='last')
 
         if drop_uninformative:
@@ -113,35 +140,33 @@ class WrdsConnection:
         elif rename_columns:
             self.dataset = self.dataset.rename(columns={'from': 'joined_sp500', 'thru': 'left_sp500'})
 
-    def head(self):
+    def head(self, n=5):
         """
         Applies the head method to the attached Pandas DataFrame.
         :return: Prints the first five rows of the DataFrame.
         """
         if not isinstance(self.dataset, DataFrame):
             raise NoDatasetError('No dataset downloaded yet. Cannot display data head.')
-        # Todo: pass through arguments.
-        return self.dataset.head()
+        return self.dataset.head(n)
 
-    def add_names(self, dataframe: DataFrame = None):
+    def add_names(self):
         """
         Adds the company name from the "NAMES" table in compustat's "COMPA" library.
-        :param dataframe: Optional, a dataframe with a gvkey column for which to provide company names.
-        :return: When a dataframe is provided, a dataframe is returned. Otherwise, will save the amended dataset back
-        to the WrdsConnection object.
         """
+        if not isinstance(self.dataset, DataFrame):
+            raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
+
         self._download_names_table()
-        if dataframe is not None:
-            dataframe = dataframe.merge(self._names_table[['gvkey', 'conm']], on='gvkey', how='left')
-            return dataframe
-        else:
-            self.dataset = self.dataset.merge(self._names_table[['gvkey', 'conm']], on='gvkey', how='left')
-            self.dataset = self.dataset.rename(columns={'conm': 'name'})
+        self.dataset = self.dataset.merge(self._names_table[['gvkey', 'conm']], on='gvkey', how='left')
+        self.dataset = self.dataset.rename(columns={'conm': 'name'})
 
     def add_ticker(self):
         """
         Adds the ticker number from the "NAMES" table in compustat's "COMPA" library.
         """
+        if not isinstance(self.dataset, DataFrame):
+            raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
+
         self._download_names_table()
         self.dataset = self.dataset.merge(self._names_table[['gvkey', 'tic']], on='gvkey', how='left')
         self.dataset = self.dataset.rename(columns={'tic': 'ticker'})
@@ -150,6 +175,9 @@ class WrdsConnection:
         """
         Adds the cusip code from the "NAMES" table in compustat's "COMPA" library.
         """
+        if not isinstance(self.dataset, DataFrame):
+            raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
+
         self._download_names_table()
         self.dataset = self.dataset.merge(self._names_table[['gvkey', 'cusip']], on='gvkey', how='left')
 
@@ -157,6 +185,9 @@ class WrdsConnection:
         """
         Adds the CIK code from the "NAMES" table in compustat's "COMPA" library.
         """
+        if not isinstance(self.dataset, DataFrame):
+            raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
+
         self._download_names_table()
         self.dataset = self.dataset.merge(self._names_table[['gvkey', 'CIK']], on='gvkey', how='left')
 
@@ -164,6 +195,9 @@ class WrdsConnection:
         """
         Adds the exit year from the "NAMES" table in compustat's "COMPA" library.
         """
+        if not isinstance(self.dataset, DataFrame):
+            raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
+
         self._download_names_table()
         self.dataset = self.dataset.merge(self._names_table[['gvkey', 'year2']], on='gvkey', how='left')
         # The year2 column provides the last year for which accounting data is available, therefore, the exit year is
@@ -175,11 +209,14 @@ class WrdsConnection:
         """
         Adds the IPO year (or year of merger) from the "NAMES" table in compustat's "COMPA" library.
         """
+        if not isinstance(self.dataset, DataFrame):
+            raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
+
         self._download_names_table()
         self.dataset = self.dataset.merge(self._names_table[['gvkey', 'ipodate']], on='gvkey', how='left')
         self.dataset = self.dataset.rename(columns={'ipodate': 'ipo_date'})
 
-    def add_industry_classifiers(self, get_GICS=False, get_SP=False):
+    def add_industry_classifiers(self, get_gics=False, get_sp=False):
         """
         Adds two common industry classifiers, SIC and NAICS, from the "NAMES" table in compustat's "COMPA" library.
         Also contains options to pull industry classifiers from the Global Industry Classification Standard (GIC) and
@@ -193,7 +230,7 @@ class WrdsConnection:
             raise NoDatasetError('No dataset downloaded yet. Cannot perform operation on dataset.')
 
         self._download_names_table()
-        if get_GICS or get_SP:
+        if get_gics or get_sp:
             self._download_company_table()
 
         # We split the operation into three parts (SIC/NAICS, GICS and S&P classification system) to leave the original
@@ -203,7 +240,7 @@ class WrdsConnection:
             self.dataset = self.dataset.merge(self._names_table[['gvkey', 'sic', 'naics']], on='gvkey', how='left')
             self.dataset.rename(columns={'sic': 'SIC', 'naics': 'NAICS'}, inplace=True)
 
-        if get_GICS and 'GICS_group' not in list(self.dataset):
+        if get_gics and 'GICS_group' not in list(self.dataset):
             self.dataset = self.dataset.merge(self._company_table[['gvkey', 'ggroup', 'gind', 'gsector', 'gsubind']],
                                               on='gvkey', how='left')
             self.dataset.rename(columns={'ggroup': 'GICS_group',
@@ -211,7 +248,7 @@ class WrdsConnection:
                                          'gsector': 'GICS_sector',
                                          'gsubind': 'GICS_subindustry'}, inplace=True)
 
-        if get_SP and 'SP_industry' not in list(self.dataset):
+        if get_sp and 'SP_industry' not in list(self.dataset):
             self.dataset = self.dataset.merge(self._company_table[['gvkey', 'spcindcd', 'spcseccd']],
                                               on='gvkey', how='left')
             self.dataset.rename(columns={'spcindcd': 'SP_industry', 'spcseccd': 'SP_sector'}, inplace=True)
@@ -226,18 +263,65 @@ class WrdsConnection:
         # Note: changing type to category sets all None values to nan again.
         self.dataset[new_columns] = self.dataset[new_columns].astype('category')
 
+    def add_executives(self, ceo_only=False):
+        """
+        Adds to your dataset the compustat the execid and year. The execid can the be used to download further data on
+        executives, via the add_executive_info method.
+        :param ceo_only: Only info on CEOs is added to the dataset.
+        """
+        if 'execid' in list(self.dataset):
+            print('Executives already added to the dataset. No information will be added.')
+            pass
+
+        self._download_executive_table()
+        join_data = self._executive_table[['execid', 'year', 'gvkey']].copy()
+        if ceo_only:
+            join_data = join_data[join_data['ceoann'] == 'CEO']
+
+        if 'year' not in list(self.dataset):
+            self.dataset = pd.merge(self.dataset, join_data, how='left', on='gvkey')
+
+        else:
+            self.dataset = pd.merge(self.dataset, join_data, how='left', on=['gvkey', 'year'])
+
+        self.dataset['year'] = self.dataset['year'].astype('Int64')
+        self.dataset.reset_index(drop=True, inplace=True)
+
+    def add_executive_info(self, add_title: bool = False, add_full_name: bool = False, add_salary: bool = False,
+                           add_bonus: bool = False, add_ceo_flag: bool = False):
+        """
+        Add various information about executives.
+        :param add_title: Full title of the position.
+        :param add_full_name: Full name of the executive.
+        :param add_salary: Salary by year.
+        :param add_bonus: Bonus by year.
+        :param add_ceo_flag: Takes the value "CEO" if individual is CEO.
+        :return:
+        """
+        if not (add_title or add_full_name or add_salary or add_bonus or add_ceo_flag):
+            warnings.warn('No option selected. Nothing will happen.', UserWarning)
+        else:
+            self.add_executives()
+
+        selections = [add_title, add_full_name, add_salary, add_bonus, add_ceo_flag]
+        all_info_columns = ['titleann', 'exec_fullname', 'salary', 'bonus', 'ceoann']
+        # A quick and elegant way to get a list of the column names we want to include.
+        selected_info_columns = list(compress(all_info_columns, selections)).extend(['execid', 'year'])
+
+        self.dataset = self.dataset.join(self._executive_table[selected_info_columns], how='inner',
+                                         on=['execid', 'year'])
+        # Todo: Change CEO column to boolean.
+
     def add_address(self):
+        """
+        Add company addresses, where available. The addresses contain four rows, separated by a newline character "\\n".
+        """
         self._download_company_table()
-        address = pd.Series.str.cat(others=[self._company_table['add1'],
-                                    self._company_table['add2'],
-                                    self._company_table['add3'],
-                                    self._company_table['add4']],
-                                    sep='\n',
-                                    na_rep=True
-                                    )
-        company_address_pairs = self._company_table[['gvkey']].concat(address)
-        # ToDo: Finish function to import address.
-        # self.dataset =
+
+        address = (self._company_table.add1 + '\n' + self._company_table.add2 + '\n' + self._company_table.add3 +
+                   '\n' + self._company_table.add4)
+        address_and_gvkey = pd.concat([address, self._company_table['gvkey']], axis=1)
+        self.dataset = pd.merge(self.dataset, address_and_gvkey, how='left', on=['gvkey'])
 
     def _download_names_table(self):
         """
@@ -256,6 +340,15 @@ class WrdsConnection:
         """
         if self._company_table is None:
             self._company_table = self.db.get_table(library='compa', table='company')
+
+    def _download_executive_table(self):
+        """
+        Pulls data from the "ANNCOMP" table in compustat's "EXECCOMP" library. The table contains information on company
+        executives, such as name, years of service, salary, bonuses, and similar data.
+        :return:
+        """
+        if self._executive_table is None:
+            self._executive_table = self.db.get_table(library='execcomp', table='anncomp')
 
     def return_dataframe(self):
         """
@@ -280,10 +373,10 @@ class WrdsConnection:
         if classification_system in ['GICS_group', 'GICS_industry', 'GICS_sector', 'GICS_subindustry'] \
                 and 'GICS_group' not in list(self.dataset):
             print('Downloading GICS Industry classifiers.')
-            self.add_industry_classifiers(get_GICS=True)
+            self.add_industry_classifiers(get_gics=True)
         if classification_system in ['SP_industry', 'SP_sector'] and 'SP_industry' not in list(self.dataset):
             print('Downloading S&P Industry classifiers.')
-            self.add_industry_classifiers(get_SP=True)
+            self.add_industry_classifiers(get_sp=True)
 
         if type(industry_code) == list:
             self.dataset = self.dataset[self.dataset[classification_system].isin(industry_code)]
